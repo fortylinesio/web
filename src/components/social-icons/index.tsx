ff0fdcb3a0fbc27f827ae56078e8a613632bda14
Almira Sagynbek <almira.sagynbek@gmail.com>
2021-02-15 15:13:41 +0600

    chore: social networks open in a new tab

--- src/components/social-icons/index.tsx
+++ src/components/social-icons/index.tsx
@@ -3,16 +3,16 @@ import './index.scss';
 export const SocialIcons = () => {
   return (
     <div className='social-icons'>
-      <a className='icon' href='https://github.com/fortylinesio' target='_blank' rel='noreferrer'>
+      <a className='icon github' href='https://github.com/fortylinesio' target='_blank' rel='noreferrer'>
         <i className='fab fa-github' />
       </a>
-      <a className='icon linkedin' href='https://www.linkedin.com/company/fortylines-io/about/'>
+      <a className='icon linkedin' href='https://www.linkedin.com/company/fortylines-io/about/' target='_blank' rel='noreferrer'>
         <i className='fab fa-linkedin' />
       </a>
-      <a className='icon telegram' href='https://web.telegram.org/#/im?p=@sherazhy'>
+      <a className='icon telegram' href='https://web.telegram.org/#/im?p=@sherazhy' target='_blank' rel='noreferrer'>
         <i className='fab fa-telegram' />
       </a>
-      <a className='icon instagram' href='https://www.instagram.com/fortylines.io/'>
+      <a className='icon instagram' href='https://www.instagram.com/fortylines.io/' target='_blank' rel='noreferrer'>
         <i className='fab fa-instagram' />
       </a>
     </div>
